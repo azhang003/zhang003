30a172fbe9fe6c7ad8247d97943936f0bf90f3a6
767320138 <zaq123456>
2022-11-14 11:56:48 +0800

    默认变更列表

---
+++ app/controller/telegram/Index.php
@@ -0,0 +1,59 @@
+<?php
+namespace app\controller;
+
+use app\BaseController;
+use Illuminate\Http\Request;
+use Telegram\Bot\Api;
+use Telegram\Bot\FileUpload\InputFile;
+use Telegram\Bot\Laravel\Facades\Telegram;
+
+class Index extends BaseController
+{
+    /**
+     * @var \Telegram\Bot\Api
+     */
+    private Api $telegram;
+
+    public function __construct(){
+        $this->telegram = new Api('5735688118:AAHu3aWay5VEbwWDRCnZ9xQqMfNYHhU0zqg');
+    }
+    public function sendmassage(){
+        $telegram = $this->telegram;
+        $response = $telegram->getUpdates();
+//        return $response;
+        return $response;
+    }
+    public function storemassage(Request $request){
+//        $request->validate([
+//            'name'=>'required',
+//            'massage'=>'required'
+//        ]);
+        $text =  "<b>Name: </b>\n"
+            . "$request->name\n"
+            . "<b>Message: </b>\n"
+            . $request->message;
+        $result = Telegram::sendMessage([
+            'chat_id' =>$request->chat_id?: '5655239373',
+            'parse_mode' => 'HTML',
+            'text' => $text
+        ]);
+        return $result;
+
+    }
+    public function storephoto(Request $request){
+        $request->validate([
+            'file' => 'file|mimes:jpeg,png,gif'
+        ]);
+
+        $photo = $request->file('file');
+
+        Telegram::sendPhoto([
+            'chat_id' => '5655239373',
+            'photo' => InputFile::createFromContents(file_get_contents($photo->getRealPath()), str_random(10) . '.' . $photo->getClientOriginalExtension()),
+            'caption' => 'Photo Image'
+        ]);
+
+        return redirect()->back();
+
+    }
+}
